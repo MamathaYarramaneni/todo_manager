3b5fb2aca366a7944abcdbdeab1be99a3047940d
MamathaYarramaneni <yarramanenimamatha2000@gmail.com>
2020-03-29 14:23:56 +0530

    level-07 submission,erb with css to todo_manager

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -3,4 +3,53 @@ class Todo < ActiveRecord::Base
     is_completed = completed ? "[X]" : "[ ]"
     "#{id}. #{due_date.to_s(:short)} #{todo_text} #{is_completed}"
   end
+
+  def self.overdue?
+    all.where("due_date < ?", Date.today)
+  end
+
+  def self.due_today?
+    all.where("due_date = ?", Date.today)
+  end
+
+  def self.due_later?
+    all.where("due_date > ?", Date.today)
+  end
+
+  def self.completed
+    all.where(completed: true)
+  end
+
+  def self.not_completed
+    all.where(completed: false)
+  end
+
+  def self.show_list
+    puts "My Todo-list\n\n"
+
+    puts "\nOverdue\n"
+    (all.where("due_date < ?", Date.today).map { |todo|
+      puts todo.to_displayable_string
+    })
+    puts "\n\n"
+
+    puts "Due Today\n"
+    (all.where("due_date = ?", Date.today).map { |todo|
+      puts todo.to_displayable_string
+    })
+    puts "\n\n"
+
+    puts "Due Later\n"
+    (all.where("due_date > ?", Date.today).map { |todo|
+      puts todo.to_displayable_string
+    })
+    puts "\n\n"
+  end
+
+  def self.mark_as_complete!(todo_id)
+    todo = Todo.find(todo_id)
+    todo.completed = true
+    todo.save
+    todo
+  end
 end
