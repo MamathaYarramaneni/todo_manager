bfdfad82b3699b0c2b032133d1c7632ed80d4213
MamathaYarramaneni <yarramanenimamatha2000@gmail.com>
2020-04-02 17:39:34 +0530

    my level-07 todo_manager submission

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -4,24 +4,20 @@ class Todo < ActiveRecord::Base
     "#{id}. #{due_date.to_s(:short)} - #{todo_text} - #{is_completed}"
   end
 
-  def self.overdue?
-    all.where("due_date < ?", Date.today)
+  def self.overdue
+    all.where("due_date < ? and (not completed)", Date.today)
   end
 
-  def self.due_today?
+  def self.due_today
     all.where("due_date = ?", Date.today)
   end
 
-  def self.due_later?
+  def self.due_later
     all.where("due_date > ?", Date.today)
   end
 
   def self.completed
     all.where(completed: true)
-  end
-
-  def self.not_completed
-    all.where(completed: false)
   end
 
   def self.show_list
