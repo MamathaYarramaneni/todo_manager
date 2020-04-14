101abbc931f4003014e34fdce0ea82eb343fb33a
MamathaYarramaneni <yarramanenimamatha2000@gmail.com>
2020-04-14 14:30:40 +0530

    updated migrations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
     )
     if new_user.save
       session[:current_user_id] = new_user.id
-      redirect_to todos_path
+      redirect_to sessions_path
     else
       flash[:error] = new_user.errors.full_messages.join(", ")
       redirect_to new_user_path
