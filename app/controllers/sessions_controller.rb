1a773eb7b168eff9f8159f678517145c51f99db6
MamathaYarramaneni <yarramanenimamatha2000@gmail.com>
2020-04-14 12:25:40 +0530

    sign in creates a new session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,14 @@
 class SessionsController < ApplicationController
+  skip_before_action :ensure_user_logged_in
+
   def new
   end
 
   def create
     user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
-      render plain: "Welcome!"
+    if user && user.authenticate(params[:password_digest])
+      session[:current_user_id] = user.id
+      redirect_to "/"
     else
       render plain: "invalid credentials"
     end
